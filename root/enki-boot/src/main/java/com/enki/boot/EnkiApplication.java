6a96ce9bebee99b006cc98059e7db0992cecf01d
esther.sanchezm <Esther.Sanch9z@gmail.com>
2022-07-09 22:01:14 +0200

    Initial strcuture multimodule (Maybe with kotlin).

---
+++ root/enki-boot/src/main/java/com/enki/boot/EnkiApplication.java
@@ -0,0 +1,13 @@
+package com.enki.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnkiApplication.class, args);
+	}
+
+}
